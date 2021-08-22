e252a6a6c10f32e0bd16255fb1bbc99e0866e00d
Lilly Oh <ohlilly@gmail.com>
2021-08-22 14:07:51 -0700

    added adyen api configuration

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const dotenv = require('dotenv')
+const { uuid } = require('uuid');
+const { Client, Config, CheckoutAPI } = require("@adyen/api-library");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '/public')))
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const config = new Config();
+config.apiKey = process.env.REACT_APP_API_KEY;
+const client = new Client({config, environment: 'TEST'});
+const checkout = new CheckoutAPI(client);
 
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
